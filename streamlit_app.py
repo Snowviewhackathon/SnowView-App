71a2e29a44036ba1b2082700fcc23e540812d664
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-14 22:59:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ df= pd.DataFrame(res, columns=['PIPELINE_NAME','PIPELINE_EXECUTOR','PIPELINE_STA
 s=df.style.set_table_styles([
                             {
                                "selector":"thead",
-                                "props":"background-color:lightgreen; color:black; border:1.3px solid black;style:bold;"
+                                "props":"background-color:lightblue; color:black; style:bold; border:1.3px solid black;"
                             },
                             {
                                "selector":"table",
