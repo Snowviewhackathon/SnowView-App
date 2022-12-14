3ab5b83804113673097ba2914f6eb752ba8da426
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-14 18:46:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,29 @@ import base64
 from urllib.error import URLError
 #from PIL import image
 
+
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    streamlit.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://cdn.pixabay.com/photo/2020/06/19/22/33/wormhole-5319067_960_720.jpg");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+    
+set_bg_hack_url() 
+
 #streamlit.title('Snow View')
 #streamlit.success('Snow View')
 #streamlit.markdown('Snowflake Process : Execution Details')
