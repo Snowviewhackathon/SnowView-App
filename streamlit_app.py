7d5795f5820c008961fc9931d172e1ba94efbcde
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 17:31:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,16 @@
 import streamlit as st
 
-def set_bg_hack_url():
-    '''
-    A function to unpack an image from url and set as bg.
-    Returns
-    -------
-    The background.
-    '''
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             #background: url("");
-             background-color: #86b6fd;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
-    
-set_bg_hack_url;
-
-def main_page(set_bg_hack_url):
+def main_page():
     st.markdown("# Main page")
     st.sidebar.markdown("# Main page")
 
-#streamlit.markdown(f'<h1 style="color:white;font-size:40px;">{"❄️Snow View❄️"}</h1>', unsafe_allow_html=True)
-    st.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
-
-#streamlit.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
-
-#streamlit.markdown(f'<h1 style="color:white;font-size:24px;">{"Snowflake Process : Execution Details"}</h1>', unsafe_allow_html=True)
-
-    #st.sidebar.markdown("# Main page")
-
 def page2():
     st.markdown("# Page 2")
-    #st.sidebar.markdown("# Page 2")
+    st.sidebar.markdown("# Page 2")
 
 def page3():
     st.markdown("# Page 3")
-    #st.sidebar.markdown("# Page 3")
+    st.sidebar.markdown("# Page 3")
 
 page_names_to_funcs = {
     "Main Page": main_page,
