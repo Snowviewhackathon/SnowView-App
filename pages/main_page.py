e1412fce1491afec7e2bc485d21c2f391e47d8a7
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 16:09:57 +0530

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
-import streamlit as st
+import streamlit
 
 
-st.sidebar.markdown("# Main page 🎈")
+ststreamlit.sidebar.markdown("# Main page 🎈")
 
 streamlit.markdown(f'<h1 style="color:#1874CD;font-size:40px;">{"Snow View"}</h1>', unsafe_allow_html=True)
 streamlit.markdown(f'<h1 style="color:#6495ED;font-size:24px;">{"Snowflake Process : Execution Details"}</h1>', unsafe_allow_html=True)
