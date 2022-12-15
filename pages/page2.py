70a691b1c5eb118764ccb076098853b0e326483a
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 16:39:07 +0530

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -19,7 +19,7 @@ def set_bg_hack_url():
          <style>
          .stApp {{
              #background: url("");
-             background-color: PaleGoldenrod;
+             background-color: #DDA0DD;
              background-size: cover
          }}
          </style>
