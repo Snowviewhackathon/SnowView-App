c2581673278e8a03b990ae91718c859a569d81f2
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 20:41:28 +0530

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -2,14 +2,7 @@ import streamlit
 
 
 streamlit.sidebar.markdown("# Main page")
-
-#streamlit.markdown(f'<h1 style="color:white;font-size:40px;">{"❄️Snow View❄️"}</h1>', unsafe_allow_html=True)
 streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
-
-#streamlit.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
-
-#streamlit.markdown(f'<h1 style="color:white;font-size:24px;">{"Snowflake Process : Execution Details"}</h1>', unsafe_allow_html=True)
-
 streamlit.markdown(
      f"""
      <style>
@@ -22,5 +15,3 @@ streamlit.markdown(
      """,
      unsafe_allow_html=True
  )
-    
-
