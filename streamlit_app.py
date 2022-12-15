de4e8905b0ee50ae2edab5b5c02d36888adf044e
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 13:38:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,14 +41,14 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed for Pipeline Execution','ERROR_DETAILS'])
+df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed for Pipeline Execution','Error Details'])
 
-#streamlit.dataframe(df,3000,1500)
+streamlit.dataframe(df,3000,1500)
 #a=df.style.set_properties(**{'border': '1.3px solid green','color': 'magenta'})
 s=df.style.set_table_styles([
                             {
                                "selector":"thead",
-                                "props":"font-weight:bold; color:#FF0000; background-color:LightSkyBlue; border:1.3px solid black;"
+                                "props":"font-weight:bold; color:#FF0000; background-color:White; border:1.3px solid black;"
                             },
                             {
                                "selector":"td",
