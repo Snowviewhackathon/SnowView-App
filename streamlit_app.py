b8faa398a211333dafa35a9accf4f38397fd2070
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 19:23:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,4 @@
 import streamlit as st
 
-def main_page():
-    st.markdown("# Main page")
-    st.sidebar.markdown("# Main page")
-
-def page2():
-    st.markdown("# Page 2")
-    st.sidebar.markdown("# Page 2")
-
-def page3():
-    st.markdown("# Page 3")
-    st.sidebar.markdown("# Page 3")
-
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
-
+st.markdown("# Main page ")
+st.sidebar.markdown("# Main page ")
