1b803566aa67d89b638bdf96c957752644b6e267
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 15:37:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ def set_bg_hack_url():
          <style>
          .stApp {{
              #background: url("");
-             background-color: purple;
+             background-color: MediumOrchid;
              background-size: cover
          }}
          </style>
