18f7424101df8a395f8109aef682ab02ad8a3027
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-15 17:37:57 +0530

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,24 +10,17 @@ streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>
 
 #streamlit.markdown(f'<h1 style="color:white;font-size:24px;">{"Snowflake Process : Execution Details"}</h1>', unsafe_allow_html=True)
 
-def set_bg_hack_url():
-    '''
-    A function to unpack an image from url and set as bg.
-    Returns
-    -------
-    The background.
-    '''
-    streamlit.markdown(
-         f"""
-         <style>
-         .stApp {{
-             #background: url("");
-             background-color: #86b6fd;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
+streamlit.markdown(
+     f"""
+     <style>
+     .stApp {{
+         #background: url("");
+         background-color: #86b6fd;
+         background-size: cover
+     }}
+     </style>
+     """,
+     unsafe_allow_html=True
+ )
     
-set_bg_hack_url() 
+
