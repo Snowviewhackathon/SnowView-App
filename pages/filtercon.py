4369cc954c88eec29797c953f72e92f98b94b675
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-16 19:14:04 +0530

    Create filtercon.py

---
+++ pages/filtercon.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Adds a UI on top of a dataframe to let viewers filter columns
+
+    Args:
+        df (pd.DataFrame): Original dataframe
+
+    Returns:
+        pd.DataFrame: Filtered dataframe
+    """
+    modify = st.checkbox("Add filters")
+
+    if not modify:
+        return df 
