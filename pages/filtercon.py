1dc8e4ecfd0103d12a5e813b323cb7065299c433
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-16 19:56:02 +0530

    Update filtercon.py

--- pages/filtercon.py
+++ pages/filtercon.py
@@ -14,20 +14,9 @@ st.write(
     and walks you through one example of how the Streamlit
     Data Science Team builds add-on functions to Streamlit.
     """
-)
+)  
 
-def dataframe():
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_cur = my_cnx.cursor() 
-    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-    my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
-    res = my_cur.fetchall()
-    df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed','Error Details'])
-    
-    return df
-
-
-def filter_dataframe(df):
+def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
     Adds a UI on top of a dataframe to let viewers filter columns
 
@@ -75,5 +64,14 @@ def filter_dataframe(df):
                 if user_text_input:
                     df = df[df[column].astype(str).str.contains(user_text_input)]
     return df
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed','Error Details'])
+    
+
 
 st.dataframe(filter_dataframe(df))
