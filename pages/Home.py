0ada935df714f091c53de7ac0ad21c7ef4aa9c42
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-16 20:12:18 +0530

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,18 +1,18 @@
 import streamlit
 
- modification_container = st.container()
-    with modification_container:
-          streamlit.sidebar.markdown("# Main page")
-          streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
-          streamlit.markdown(
-               f"""
-               <style>
-               .stApp {{
-                   #background: url("");
-                   background-color: #86b6fd;
-                   background-size: cover
-               }}
-               </style>
-               """,
-               unsafe_allow_html=True
-           )
+modification_container = st.container()
+with modification_container:
+    streamlit.sidebar.markdown("# Main page")
+    streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
+    streamlit.markdown(
+         f"""
+         <style>
+         .stApp {{
+             #background: url("");
+             background-color: #86b6fd;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
