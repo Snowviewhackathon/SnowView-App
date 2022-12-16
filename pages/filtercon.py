75686fcbfe16b14e8433f95ae2e916f322a55a75
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-16 21:17:05 +0530

    Update filtercon.py

--- pages/filtercon.py
+++ pages/filtercon.py
@@ -7,15 +7,7 @@ from pandas.api.types import (
     is_datetime64_any_dtype,
     is_numeric_dtype,
     is_object_dtype,
-)
-
-st.title("Auto Filter Dataframes in Streamlit")
-st.write(
-    """This app accomodates the blog [here](<https://blog.streamlit.io/auto-generate-a-dataframe-filtering-ui-in-streamlit-with-filter_dataframe/>)
-    and walks you through one example of how the Streamlit
-    Data Science Team builds add-on functions to Streamlit.
-    """
-)  
+) 
 
 def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """
