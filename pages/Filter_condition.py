e552a98e88671bf0ff1a321d04aa887859033591
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-17 12:22:33 +0530

    Update Filter_condition.py

--- pages/Filter_condition.py
+++ pages/Filter_condition.py
@@ -71,7 +71,19 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed','Error Details'])
-    
+st.markdown(
+     f"""
+     <style>
+     .stApp {{
+         #background: url("");
+         background-color: #86b6fd;
+         background-size: cover
+     }}
+     </style>
+     """,
+     unsafe_allow_html=True
+ )
+
 
 
 st.dataframe(filter_dataframe(df))
