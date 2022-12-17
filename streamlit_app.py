ba292fd23ff9a3e4fce05d2fee2c84a7888478ad
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-17 17:33:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,13 @@ import streamlit
 
 modification_container = streamlit.container()
 with modification_container:
-    #streamlit.sidebar.markdown("# Main page")
-    streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        streamlit.write(' ')
+    with col2:
+        streamlit.markdown("<h1 style='text-align: center; color: white;font-size:80px'>❄️Snow View❄️</h1>", unsafe_allow_html=True)
+    with col3:
+        streamlit.write(' ')
     streamlit.markdown(
          f"""
          <style>
@@ -16,4 +21,3 @@ with modification_container:
          """,
          unsafe_allow_html=True
      )
-
