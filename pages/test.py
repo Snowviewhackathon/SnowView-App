f09a7fa997082c22baa86011355fdacb16f6d04e
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 16:38:41 +0530

    Create test.py

---
+++ pages/test.py
@@ -0,0 +1,14 @@
+import streamlit
+# Import pandas library
+import pandas as pd
+ 
+# initialize list of lists
+data = [['tom', 10], ['nick', 15], ['juli', 14]]
+ 
+# Create the pandas DataFrame
+df = pd.DataFrame(data, columns=['Name', 'Age'])
+a=df.style.set_table_styles(
+    [{'selector': 'tr:hover',
+      'props': [('background-color', 'yellow')]}]
+)  
+streamlit.dataframe(a)
