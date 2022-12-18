da4e9ebdb4e1f8e19374028773fbc9dcfe101327
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 13:55:57 +0530

    Update Usage Metrics(last 7 days).py

--- pages/Usage Metrics(last 7 days).py
+++ pages/Usage Metrics(last 7 days).py
@@ -87,8 +87,8 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed','Error Details'])
-st.markdown(f'<h1 style="color:#FFFFFF;font-size:48px;">{"❄️  SnowView"}</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="color:#FFFFFF;font-size:24px;">{"Snowflake Process : Last 7 days History"}</h1>', unsafe_allow_html=True)    
+st.markdown(f'<h1 style="color:#FFFFFF;font-size:48px;">{"❄️ SnowView"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:#FFFFFF;font-size:24px;">{"Usage Metrics (last 7 days)"}</h1>', unsafe_allow_html=True)    
 st.markdown(
      f"""
      <style>
