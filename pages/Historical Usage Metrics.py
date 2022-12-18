92b708f425805332182b53b1760b6827f7c69e93
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 14:11:18 +0530

    Update Historical Usage Metrics.py

--- pages/Historical Usage Metrics.py
+++ pages/Historical Usage Metrics.py
@@ -86,9 +86,9 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time','Credits Consumed','Error Details'])
-st.markdown(f'<h1 style="color:#FFFFFF;font-size:48px;">{"❄️ SnowView"}</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="color:#FFFFFF;font-size:24px;">{"Historical Usage Metrics"}</h1>', unsafe_allow_html=True)   
+df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+st.markdown(f'<h1 style="color:#FFFFFF;font-size:48px;">{"❄️SnowView"}</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="color:#FFFFFF;font-size:24px;text-align: center;">{"Historical Usage Metrics"}</h1>', unsafe_allow_html=True)   
 st.markdown(
      f"""
      <style>
