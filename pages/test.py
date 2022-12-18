1b467bee372d55cb331002b84ccba70f896013bd
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 16:57:15 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -9,7 +9,7 @@ data = [['tom', 10], ['nick', 15], ['juli', 14]]
 df = pd.DataFrame(data, columns=['Name', 'Age'])
 df = df.style.set_properties(**{
     'background-color': 'green',
-    'font-size': '20pt',
+    'font-size': '50pt',
 })
 #df.set_table_styles([cell_hover, index_names, headers])
 streamlit.dataframe(df)
