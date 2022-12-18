dab5279aa577521c061bbbde439f1169935b234c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 17:01:09 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,11 +7,24 @@ data = [['tom', 10], ['nick', 15], ['juli', 14]]
  
 # Create the pandas DataFrame
 df = pd.DataFrame(data, columns=['Name', 'Age'])
-df = df.style.set_properties(**{
-    'background-color': 'green',
-    'font-size': '50pt',
-})
+s=df.style.set_table_styles([
+                            {
+                               "selector":"thead",
+                                "props":"font-weight:bold; color:#FF0000; background-color:White; border:1.3px solid black;"
+                            },
+                            {
+                               "selector":"td",
+                                "props":"font-size:20px"
+                            },
+                          
+                            {
+                               "selector":"stTable",
+                                "props":"width:100%;"
+                            },
+
+                       ])
+
 #df.set_table_styles([cell_hover, index_names, headers])
-streamlit.dataframe(df)
+streamlit.dataframe(s)
 
 #streamlit.dataframe(df.style.set_table_styles([cell_hover, index_names, headers]))
