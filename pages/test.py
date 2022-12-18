1ed35f463ef25ac94b8f49813b1e29e02a0b4c5f
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-18 16:47:06 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,10 +7,18 @@ data = [['tom', 10], ['nick', 15], ['juli', 14]]
  
 # Create the pandas DataFrame
 df = pd.DataFrame(data, columns=['Name', 'Age'])
+cell_hover = {  # for row hover use <tr> instead of <td>
+    'selector': 'td:hover',
+    'props': [('background-color', '#ffffb3')]
+}
+index_names = {
+    'selector': '.index_name',
+    'props': 'font-style: italic; color: darkgrey; font-weight:normal;'
+}
+headers = {
+    'selector': 'th:not(.index_name)',
+    'props': 'background-color: #000066; color: white;'
+}
+df.set_table_styles([cell_hover, index_names, headers])
 
-streamlit.dataframe(df.style.set_table_styles({
-    'A': [{'selector': '',
-           'props': [('color', 'red')]}],
-    'B': [{'selector': 'td',
-           'props': 'color: blue;'}]
-}, overwrite=False))
+streamlit.dataframe(df.set_table_styles([cell_hover, index_names, headers]))
