b43baccd6fb6a92eca9ed0ddabd41f7966314d7a
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-19 16:59:44 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,6 +1,7 @@
 import streamlit as st
 # Import pandas library
 import pandas as pd
+import seaborn as sns
  
 # initialize list of lists
 data = [['tom', 10], ['nick', 15], ['juli', 14]]
@@ -8,8 +9,18 @@ data = [['tom', 10], ['nick', 15], ['juli', 14]]
 # Create the pandas DataFrame
 df = pd.DataFrame(data, columns=['Name', 'Age'])
 
+
+ 
+# Declaring the cm variable by the
+# color palette from seaborn
+cm = sns.light_palette("green", as_cmap=True)
+ 
+# Visualizing the DataFrame with set precision
+print("\nModified Stlying DataFrame:")
+#df.style.background_gradient(cmap=cm).set_precision(2)
+
  
 #st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'})))
-st.dataframe(df.style.set_properties(**{'background-color': 'black','color': 'green','font-size':'50rem'}))                                                       
+st.dataframe(dfdf.style.background_gradient(cmap=cm).set_precision(2))                                                       
 
 
