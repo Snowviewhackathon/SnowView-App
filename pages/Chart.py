2e6b98124ed3368a7908f250dd727d2126f8ae71
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-19 23:43:24 +0530

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -103,7 +103,9 @@ st.markdown(
  )
 #st.dataframe(filter_dataframe(df))
 #st.dataframe(df)
-if st.button('Credits Consumed Chart'):   
+result =st.button('Credits Consumed Chart')
+st.write(result)
+if result:   
 st.line_chart(df, x="Pipeline Start Time", y=["Credits Consumed"])
-else:
-   st.line_chart(df, x="Pipeline Start Time", y=["Pipeline Execution Time (in seconds)"])
+#else:
+ #  st.line_chart(df, x="Pipeline Start Time", y=["Pipeline Execution Time (in seconds)"])
