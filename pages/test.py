af97066cf398f970e38f33a49625d0b9290a402c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 11:07:01 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,12 +7,14 @@ data = [['tom', 10], ['nick', 15], ['juli', 14]]
 # Create the pandas DataFrame
 df = pd.DataFrame(data, columns=['Name', 'Age'])
 
+st.dataframe(df)
+
 st.dataframe(
     df.style.applymap(color_negative_red).apply(
         highlight_max, color="darkorange", axis=0
     )
 )
-st.dataframe(df)
+#st.dataframe(df)
 
 #st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'}))                                                   
 
