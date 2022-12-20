a823d35a055d634d93c83b6bfe6caa71dd55b59d
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 14:18:33 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -6,10 +6,14 @@ import numpy as np
 def color_negative(v, color):
     return f"color: {color};" if v < 0 else None
 df = pd.DataFrame(np.random.randn(5, 2), columns=["A", "B"])
-a=df.style.applymap(color_negative, color='red')  
+#a=df.style.applymap(color_negative, color='red')  
+b=df.style.applymap(color_negative, color='red', subset="A")
+ 
+#df.style.applymap(color_negative, color='red', subset=["A", "B"])
+  
 
 #st.dataframe(   df.style.applymap(color_negative_red).apply(highlight_max, color="darkorange", axis=0))
-st.dataframe(a)
+st.dataframe(b)
 #st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'}))                                                   
 
 
