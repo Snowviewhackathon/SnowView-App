83dfe7dd15073281aa5153789b190b1651365436
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 11:08:12 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -10,8 +10,8 @@ df = pd.DataFrame(data, columns=['Name', 'Age'])
 st.dataframe(df)
 
 st.dataframe(
-    df.style.applymap(blue).apply(
-        highlight_max, color="pink", axis=0
+    df.style.applymap(lightblue).apply(
+        highlight_max, color="lightpink", axis=0
     )
 )
 #st.dataframe(df)
