80fdb08411eb53df6523821685c3a9a148ae036c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 11:10:35 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,6 +2,28 @@ import streamlit as st
 # Import pandas library
 import pandas as pd
 
+def color_negative_red(val):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    color = "red" if val < 0 else "black"
+    return "color: %s" % color
+
+
+def highlight_max(data, color="yellow"):
+    """highlight the maximum in a Series or DataFrame"""
+    attr = "background-color: {}".format(color)
+    if data.ndim == 1:  # Series from .apply(axis=0) or axis=1
+        is_max = data == data.max()
+        return [attr if v else "" for v in is_max]
+    else:  # from .apply(axis=None)
+        is_max = data == data.max().max()
+        return pd.DataFrame(
+            np.where(is_max, attr, ""), index=data.index, columns=data.columns
+        )
+
 data = [['tom', 10], ['nick', 15], ['juli', 14]]
  
 # Create the pandas DataFrame
@@ -10,8 +32,8 @@ df = pd.DataFrame(data, columns=['Name', 'Age'])
 st.dataframe(df)
 
 st.dataframe(
-    df.style.applymap(lightblue).apply(
-        highlight_max, color="lightpink", axis=0
+    df.style.applymap(color_negative_red).apply(
+        highlight_max, color="darkorange", axis=0
     )
 )
 #st.dataframe(df)
