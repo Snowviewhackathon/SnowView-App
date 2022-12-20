07b4c51de1429c655dd868ae421a5734ecf3db4a
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 10:59:23 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,38 +2,6 @@ import streamlit as st
 # Import pandas library
 import pandas as pd
 
-def HTML_with_style(df, style=None, random_id=None):
-    from IPython.display import HTML
-    import numpy as np
-    import re
-
-    df_html = df.to_html()
-
-    if random_id is None:
-        random_id = 'id%d' % np.random.choice(np.arange(1000000))
-
-    if style is None:
-        style = """
-        <style>
-            table#{random_id} {{color: blue}}
-        </style>
-        """.format(random_id=random_id)
-    else:
-        new_style = []
-        s = re.sub(r'</?style>', '', style).strip()
-        for line in s.split('\n'):
-                line = line.strip()
-                if not re.match(r'^table', line):
-                    line = re.sub(r'^', 'table ', line)
-                new_style.append(line)
-        new_style = ['<style>'] + new_style + ['</style>']
-
-        style = re.sub(r'table(#\S+)?', 'table#%s' % random_id, '\n'.join(new_style))
-
-    df_html = re.sub(r'<table', r'<table id=%s ' % random_id, df_html)
-
-    return HTML(style + df_html)
-# initialize list of lists
 data = [['tom', 10], ['nick', 15], ['juli', 14]]
  
 # Create the pandas DataFrame
@@ -42,8 +10,6 @@ df = pd.DataFrame(data, columns=['Name', 'Age'])
 
 
 
- 
-#st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'})))
-st.dataframe( HTML_with_style(df))                                                       
+#st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'}))                                                   
 
 
