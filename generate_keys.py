a42c60f6c97c66b84260a877092081ad8d1eb71e
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 19:10:49 +0530

    Create generate_keys.py

---
+++ generate_keys.py
@@ -0,0 +1,17 @@
+### Generate keys
+
+
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ["Aniket", "Venu"]
+usernames = ["Aniket_S", "Venu_B"]
+passwords = ["1234", "123"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
