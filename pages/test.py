d0747eefd0c9dbc458fe8f0e141f91c2b2f642b6
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-20 14:30:33 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -7,12 +7,12 @@ def color_negative(v, color):
     return f"color: {color};" if v < 0 else None
 df = pd.DataFrame(np.random.randn(5, 2), columns=["A", "B"])
 #a=df.style.applymap(color_negative, color='red')  
-b=df.style.applymap(color_negative, color='red', subset="A")
+#b=df.style.applymap(color_negative, color='red', subset="A")
  
-c=df.style.applymap(color_negative, color='red', subset=["A", "B"])
+#c=df.style.applymap(color_negative, color='red', subset=["A", "B"])
 
-d=df.style.applymap(color_negative, color='red',subset=([0,1,2], slice(None)))  
-e=df.style.applymap(color_negative, color='red', subset=(slice(0,5,2), "A"))
+#d=df.style.applymap(color_negative, color='red',subset=([0,1,2], slice(None)))  
+#e=df.style.applymap(color_negative, color='red', subset=(slice(0,5,2), "A"))
 
 cell_hover = {  # for row hover use <tr> instead of <td>
     'selector': 'td:hover',
@@ -36,7 +36,10 @@ f=df.style.set_table_styles(
   
 
 
-st.dataframe(f)
+st.dataframe(df.style.set_table_styles(
+    [{'selector': 'tr:hover',
+      'props': [('background-color', 'yellow')]}]
+)  )
 #st.dataframe(filter_dataframe(df.style.set_properties(**{'font-size':'24pt'}))                                                   
 
 
