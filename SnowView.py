4785065d442d950ed528e60678ed39290e4c5b25
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-24 14:39:34 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -39,38 +39,39 @@ def user_auth():
     print(">>>>>>>>>>>>>>",data)
     return data
    
+def user_auth_one(name, authentication_status, username,authenticator):
+    if authentication_status == False:
+        st.error("Username/password is incorrect")
+        st.markdown(hide_bar, unsafe_allow_html=True)
 
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-    st.markdown(hide_bar, unsafe_allow_html=True)
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-    st.markdown(hide_bar, unsafe_allow_html=True)
-
-
-if authentication_status:
-    # # ---- SIDEBAR ----
-    st.sidebar.title(f"Welcome {name}")
-    # st.sidebar.header("select page here :")
-    st.write("# Welcome to SnowView!..")
-
-    ###about ....
-    #st.subheader("Introduction :")
-    #st.text("1. \n2. \n3. \n4. \n5. \n")
-
-    #st.sidebar.success("Select a page above.")
-
-    ###---- HIDE STREAMLIT STYLE ----
-    hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_st_style, unsafe_allow_html=True)
+    if authentication_status == None:
+        st.warning("Please enter your username and password")
+        st.markdown(hide_bar, unsafe_allow_html=True)
 
 
-    authenticator.logout("Logout", "sidebar")
+    if authentication_status:
+        # # ---- SIDEBAR ----
+        st.sidebar.title(f"Welcome {name}")
+        # st.sidebar.header("select page here :")
+        st.write("# Welcome to SnowView!..")
+
+        ###about ....
+        #st.subheader("Introduction :")
+        #st.text("1. \n2. \n3. \n4. \n5. \n")
+
+        #st.sidebar.success("Select a page above.")
+
+        ###---- HIDE STREAMLIT STYLE ----
+        hide_st_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    header {visibility: hidden;}
+                    </style>
+                    """
+        st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+        authenticator.logout("Logout", "sidebar")
+     user_auth_one()  
 st.snow()
