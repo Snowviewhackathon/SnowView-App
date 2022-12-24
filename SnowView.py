fff266ddd602084470e8d8e5fe7d8493bb0c431c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-24 22:09:38 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -1,97 +1,79 @@
-import yaml
-import streamlit as st
-from yaml.loader import SafeLoader
-import streamlit.components.v1 as components
+import pickle
+from pathlib import Path
 
-from auth.hasher import Hasher
-from auth.authenticate import Authenticate
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
-_RELEASE = True
 
-if not _RELEASE:
-    # hashed_passwords = Hasher(['123', '456']).generate()
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:", layout="wide")
 
-    # Loading config file
-    with open('../config.yaml') as file:
-        config = yaml.load(file, Loader=SafeLoader)
+hide_bar= """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        visibility:hidden;
+        width: 0px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        visibility:hidden;
+    }
+    </style>
+"""
 
-    # Creating the authenticator object
-    authenticator = Authenticate(
-        config['credentials'],
-        config['cookie']['name'], 
-        config['cookie']['key'], 
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
+# --- USER AUTHENTICATION ---
+names = ["Aniket", "Venu"]
+usernames = ["Aniket_S", "Venu_B"]
 
-    # creating a login widget
-    name, authentication_status, username = authenticator.login('Login', 'main')
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "SIPL_dashboard", "abcdef")
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+    st.markdown(hide_bar, unsafe_allow_html=True)
+
+
+if authentication_status:
+    # # ---- SIDEBAR ----
+    st.sidebar.title(f"Welcome {name}")
+    # st.sidebar.header("select page here :")
+    st.write("# Welcome to SnowView!..")
+
+    ###about ....
+    #st.subheader("Introduction :")
+    #st.text("1. \n2. \n3. \n4. \n5. \n")
+
+    #st.sidebar.success("Select a page above.")
+
+    ###---- HIDE STREAMLIT STYLE ----
+    hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
+
     if authentication_status:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{name}*')
-        st.title('Some content')
-    elif authentication_status == False:
-        st.error('Username/password is incorrect')
-    elif authentication_status == None:
-        st.warning('Please enter your username and password')
-
-    # Creating a password reset widget
-    if authentication_status:
-        try:
-            if authenticator.reset_password(username, 'Reset password'):
-                st.success('Password modified successfully')
-        except Exception as e:
-            st.error(e)
-
-    # Creating a new user registration widget
     try:
-        if authenticator.register_user('Register user', preauthorization=False):
-            st.success('User registered successfully')
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
     except Exception as e:
         st.error(e)
 
-    # Creating a forgot password widget
-    try:
-        username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
-        if username_forgot_pw:
-            st.success('New password sent securely')
-            # Random password to be transferred to user securely
-        elif username_forgot_pw == False:
-            st.error('Username not found')
-    except Exception as e:
-        st.error(e)
 
-    # Creating a forgot username widget
-    try:
-        username_forgot_username, email_forgot_username = authenticator.forgot_username('Forgot username')
-        if username_forgot_username:
-            st.success('Username sent securely')
-            # Username to be transferred to user securely
-        elif username_forgot_username == False:
-            st.error('Email not found')
-    except Exception as e:
-        st.error(e)
-
-    # Creating an update user details widget
-    if authentication_status:
-        try:
-            if authenticator.update_user_details(username, 'Update user details'):
-                st.success('Entries updated successfully')
-        except Exception as e:
-            st.error(e)
-
-    # Saving config file
-    with open('../config.yaml', 'w') as file:
-        yaml.dump(config, file, default_flow_style=False)
-
-    # Alternatively you may use st.session_state['name'], st.session_state['authentication_status'], 
-    # and st.session_state['username'] to access the name, authentication_status, and username. 
-
-    # if st.session_state['authentication_status']:
-    #     authenticator.logout('Logout', 'main')
-    #     st.write(f'Welcome *{st.session_state["name"]}*')
-    #     st.title('Some content')
-    # elif st.session_state['authentication_status'] == False:
-    #     st.error('Username/password is incorrect')
-    # elif st.session_state['authentication_status'] == None:
-    #     st.warning('Please enter your username and password')
+    authenticator.logout("Logout", "sidebar")
+st.snow()
