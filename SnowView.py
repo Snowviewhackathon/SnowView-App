df7e03222f056f96e0b6a7d81a4739426164b46f
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-24 14:55:26 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -6,67 +6,68 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
+def user_auth():
+    # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+    st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:", layout="wide")
 
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:", layout="wide")
+    hide_bar= """
+        <style>
+        [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+            visibility:hidden;
+            width: 0px;
+        }
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+            visibility:hidden;
+        }
+        </style>
+    """
 
-hide_bar= """
-    <style>
-    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
-        visibility:hidden;
-        width: 0px;
-    }
-    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
-        visibility:hidden;
-    }
-    </style>
-"""
+    # --- USER AUTHENTICATION ---
+    names = ["Aniket", "Venu"]
+    usernames = ["Aniket_S", "Venu_B"]
 
-# --- USER AUTHENTICATION ---
-names = ["Aniket", "Venu"]
-usernames = ["Aniket_S", "Venu_B"]
+    # load hashed passwords
+    file_path = Path(__file__).parent / "hashed_pw.pkl"
+    with file_path.open("rb") as file:
+        hashed_passwords = pickle.load(file)
 
-# load hashed passwords
-file_path = Path(__file__).parent / "hashed_pw.pkl"
-with file_path.open("rb") as file:
-    hashed_passwords = pickle.load(file)
+    authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+        "SIPL_dashboard", "abcdef")
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    "SIPL_dashboard", "abcdef")
+    name, authentication_status, username = authenticator.login("Login", "main")
 
-name, authentication_status, username = authenticator.login("Login", "main")
+    if authentication_status == False:
+        st.error("Username/password is incorrect")
+        st.markdown(hide_bar, unsafe_allow_html=True)
 
-if authentication_status == False:
-    st.error("Username/password is incorrect")
-    st.markdown(hide_bar, unsafe_allow_html=True)
-
-if authentication_status == None:
-    st.warning("Please enter your username and password")
-    st.markdown(hide_bar, unsafe_allow_html=True)
-
-
-if authentication_status:
-    # # ---- SIDEBAR ----
-    st.sidebar.title(f"Welcome {name}")
-    # st.sidebar.header("select page here :")
-    st.write("# Welcome to SnowView!..")
-
-    ###about ....
-    #st.subheader("Introduction :")
-    #st.text("1. \n2. \n3. \n4. \n5. \n")
-
-    #st.sidebar.success("Select a page above.")
-
-    ###---- HIDE STREAMLIT STYLE ----
-    hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_st_style, unsafe_allow_html=True)
+    if authentication_status == None:
+        st.warning("Please enter your username and password")
+        st.markdown(hide_bar, unsafe_allow_html=True)
 
 
-    authenticator.logout("Logout", "sidebar")
+    if authentication_status:
+        # # ---- SIDEBAR ----
+        st.sidebar.title(f"Welcome {name}")
+        # st.sidebar.header("select page here :")
+        st.write("# Welcome to SnowView!..")
+
+        ###about ....
+        #st.subheader("Introduction :")
+        #st.text("1. \n2. \n3. \n4. \n5. \n")
+
+        #st.sidebar.success("Select a page above.")
+
+        ###---- HIDE STREAMLIT STYLE ----
+        hide_st_style = """
+                    <style>
+                    #MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    header {visibility: hidden;}
+                    </style>
+                    """
+        st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+        authenticator.logout("Logout", "sidebar")
+    return authenticator
 st.snow()
