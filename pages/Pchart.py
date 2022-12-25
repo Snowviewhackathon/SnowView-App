9f5ea818c7eb4fbd6aad824726382ce8ad9007a9
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-25 12:51:55 +0530

    Update Pchart.py

--- pages/Pchart.py
+++ pages/Pchart.py
@@ -1,6 +1,17 @@
 import plotly.express as px
 import streamlit as st
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-fig = px.pie(df, values='pop', names='country', title='Population of European continent')
+import plotly.express as px
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+
+# This dataframe has 244 lines, but 4 distinct values for `day`
+#df = px.data.res()
+fig = px.pie(df, values='Credits Consumed', names='Pipeline Name')
+#fig.show()
 st.write(fig)
