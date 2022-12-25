883628381386409e6872c3e93f6fcd708f1ed991
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-25 12:57:37 +0530

    Update Pchart.py

--- pages/Pchart.py
+++ pages/Pchart.py
@@ -13,6 +13,6 @@ df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Sta
 
 # This dataframe has 244 lines, but 4 distinct values for `day`
 #df = px.data.res()
-fig = px.pie(df, values='Credits Consumed', names='Pipeline Name')
+fig = px.pie(df, values='Pipeline Status', names='Pipeline Name')
 #fig.show()
 st.write(fig)
