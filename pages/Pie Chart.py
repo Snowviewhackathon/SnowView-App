50d44d407d1e82b219b9e0ce2882cf27e6d1bcaf
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-26 19:53:11 +0530

    Update Pie Chart.py

--- pages/Pie Chart.py
+++ pages/Pie Chart.py
@@ -111,14 +111,41 @@ st.markdown(
  #   st.write('test')
 ##st.area_chart(df, x="Pipeline Start Time", y="Credits Consumed")
 #else:
-##st.line_chart(df, x="Pipeline Name", y="Credits Consumed")
+st.line_chart(df, x='Pipeline Name', y='Credits Consumed')
 
-y = df.columns
+##y = df.columns
 #x = pd.DataFrame(res, columns=['Pipeline Status'])
 
 # mylabels = ["Success", "Failed"]
-plt.pie(y, 'Pipeline Status')
+##plt.pie(y, 'Pipeline Status')
 #, labels = mylabels, startangle = 90)
 
 
 
+
+
+
+
+##import plotly.express as px
+##import streamlit as st
+##import snowflake.connector
+##import pandas as pd
+
+
+##my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+##my_cur = my_cnx.cursor() 
+##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+##my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
+##res = my_cur.fetchall()
+##df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+
+##fig = px.pie(df, values='Credits Consumed', names='Pipeline Name',title='Pipeline Name and Credits Consumed')
+##fig1 = px.pie(df, values='Credits Consumed', names='Pipeline Status',title='Pipeline Status and Credits Consumed')
+##fig.update_traces(textposition='inside')
+#fig.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
+#fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+#fig.show()
+##st.write(fig)
+
+
+st.write(fig1)
