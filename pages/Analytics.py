e520e755957c8e74bc539ad98ee55c66491daf54
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-26 13:26:08 +0530

    Update Analytics.py

--- pages/Analytics.py
+++ pages/Analytics.py
@@ -1,6 +1,7 @@
 import snowflake.connector
 import streamlit as st
 import pandas as pd
+import numpy as np
 import streamlit.components.v1 as components
 from pandas.api.types import (
     is_categorical_dtype,
