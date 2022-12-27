34da0e916f59d4321f4766074dc9d350d13b4ea9
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-27 17:16:31 +0530

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -1,22 +1,18 @@
 import snowflake.connector
 import streamlit as st
 import pandas as pd
-import streamlit.components.v1 as components
-from pandas.api.types import (
-    is_categorical_dtype,
-    is_datetime64_any_dtype,
-    is_numeric_dtype,
-    is_object_dtype,
-) 
 
 
-#my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-#my_cur = my_cnx.cursor() 
+
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
-#res = my_cur.fetchall()
-#df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline_Start_Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits_Consumed','Error Details'])
-#st.line_chart(df, x="Pipeline_Start_Time", y="Credits_Consumed")
+my_cur.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['Pipeline_Start_Time','Credits_Consumed'])
+st.line_chart(df)
 #st.area_chart(df, x="Pipeline Name", y="Credits Consumed")
 
 import numpy as np
