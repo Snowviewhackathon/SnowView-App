b1ff8be2ce12c95dd03c3a3afab3fdd42b3ae945
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-27 17:10:28 +0530

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -10,16 +10,15 @@ from pandas.api.types import (
 ) 
 
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor() 
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
-res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline_Start_Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits_Consumed','Error Details'])
-st.line_chart(df, x="Pipeline_Start_Time", y="Credits_Consumed")
+#my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+#my_cur = my_cnx.cursor() 
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
+#res = my_cur.fetchall()
+#df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline_Start_Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits_Consumed','Error Details'])
+#st.line_chart(df, x="Pipeline_Start_Time", y="Credits_Consumed")
 #st.area_chart(df, x="Pipeline Name", y="Credits Consumed")
 
-import pandas as pd
 import numpy as np
 
 chart_data = pd.DataFrame(
