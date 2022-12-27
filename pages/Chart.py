a0f522871f5575e41a07f4c29dc5162f43ac953f
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-27 18:13:31 +0530

    Update Chart.py

--- pages/Chart.py
+++ pages/Chart.py
@@ -16,8 +16,9 @@ res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['PIPELINE_NAME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
 st.write(df)
 #st.line_chart(df, x=['Pipeline_Start_Time'], y=['Credits_Consumed'])
-st.line_chart(df)
+#st.line_chart(df)
 #st.area_chart(df)
+st.write(df_days_calories.plot( 'PIPELINE_NAME' , 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION' ))
 
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
