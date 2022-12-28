a68f63bc22194d85239ef0483a89225d9dbfdc22
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 21:19:44 +0530

    Update Analytics - Pipeline Execution time.py

--- pages/Analytics - Pipeline Execution time.py
+++ pages/Analytics - Pipeline Execution time.py
@@ -1,4 +1,3 @@
-
 import snowflake.connector
 import streamlit as st
 st.set_page_config(layout="wide")
@@ -17,3 +16,23 @@ fig.update_yaxes(titlefont=dict(color='crimson',size=10))
 
    
 st.write(fig)
+
+
+my_cur2 = my_cnx.cursor() 
+my_cur2.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur2.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
+res2 = my_cur2.fetchall()
+df2= pd.DataFrame(res2, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+
+
+fig1 = px.pie(df2, values='Pipeline Execution Time (in seconds)', names='Pipeline Name',title='Analytics for Pipeline Execution time')
+fig1.update_traces(textposition='inside')
+fig1.update_layout(legend_title_text='Pipeline Names')
+fig1.update_layout(showlegend=True, legend=dict(
+    title_font_family='Courier New',
+    font=dict(
+        size=8
+    )
+))
+
+st.write(fig1)
