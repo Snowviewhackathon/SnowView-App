fe99df19e1f5f5c57758bf5edf47ef8dae0aa479
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 18:19:26 +0530

    Update testNew.py

--- pages/testNew.py
+++ pages/testNew.py
@@ -11,9 +11,9 @@ my_cur.execute("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['PIPELINE NAME','CREDITS CONSUMED'])
 fig = px.line(df, x="PIPELINE NAME", y="CREDITS CONSUMED", title='Analytics for Credits consumed')
-#fig.update_xaxes(ticks="inside")
 fig.update_xaxes(titlefont=dict(color='crimson',size=10),tickfont=dict(family='Rockwell', size=5))
-#fig.update_yaxes(ticks="inside", col=1)
+fig.update_yaxes(titlefont=dict(color='crimson',size=10))
+
    
 st.write(fig)
 
