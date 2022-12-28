ae36a587d541d4546a3316dcb390cbe7986d32aa
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 16:53:37 +0530

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -1,1 +1,20 @@
+import snowflake.connector
+import streamlit as st
+st.set_page_config(layout="wide")
+import pandas as pd
+import plotly.express as px
 
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['PIPELINE_NAME','PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+fig = px.line(
+    data_frame = df
+    ,x = 'PIPELINE_START_TIME'
+    ,y = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'
+    ,color = 'PIPELINE_NAME'
+)
+ 
+st.write(fig)
