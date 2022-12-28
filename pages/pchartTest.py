1eaad1063e1d689c865c259b4302ac55226ac030
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 10:09:40 +0530

    Create pchartTest.py

---
+++ pages/pchartTest.py
@@ -0,0 +1,23 @@
+import snowflake.connector
+import streamlit as st
+st.set_page_config(layout="wide")
+import pandas as pd
+
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur_new = my_cnx.cursor() 
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
+my_cur.execute("SELECT PIPELINE_NAME,Pipeline Status,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+my_cur_new.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+res = my_cur.fetchall()
+res_new=my_cur_new.fetchall()
+#df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+df= pd.DataFrame(res, columns=['PIPELINE_NAME','Pipeline Status','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+df_new= pd.DataFrame(res_new, columns=[' PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+df = df.set_index('PIPELINE_NAME')
+df_new = df_new.set_index(' PIPELINE_START_TIME')
+st.line_chart(df)
+#st.line_chart(data=df,x="value1",y="value2")
+st.area_chart(df_new)
