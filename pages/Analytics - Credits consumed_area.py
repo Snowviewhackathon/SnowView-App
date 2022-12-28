a1a42399eba48c6c41246a859ad6831f4911dbb4
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 18:59:06 +0530

    Update Analytics - Credits consumed_area.py

--- pages/Analytics - Credits consumed_area.py
+++ pages/Analytics - Credits consumed_area.py
@@ -6,10 +6,10 @@ import pandas as pd
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur_new = my_cnx.cursor() 
-my_cur_new.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+my_cur_new.execute("SELECT PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW ")
 res_new=my_cur_new.fetchall()
-df_new= pd.DataFrame(res_new, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME'])
-fig = px.area(df_new, x='PIPELINE NAME', y='PIPELINE EXECUTION TIME',title='Analytics for Credits consumed_area_chart')
+df_new= pd.DataFrame(res_new, columns=['PIPELINE EXECUTION TIME','CREDITS CONSUMED'])
+fig = px.area(df_new, x='PIPELINE EXECUTION TIME', y='CREDITS CONSUMED',title='Analytics for Credits consumed_area_chart')
 fig.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=5))
 fig.update_yaxes(titlefont=dict(color='crimson',size=10))
 
