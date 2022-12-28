f4df258073a3f0e1c21176b6cc8e3eeedf6f67bb
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 18:26:38 +0530

    Update Analytics - Pipeline Execution time.py

--- pages/Analytics - Pipeline Execution time.py
+++ pages/Analytics - Pipeline Execution time.py
@@ -1,1 +1,19 @@
 
+import snowflake.connector
+import streamlit as st
+st.set_page_config(layout="wide")
+import pandas as pd
+import plotly.express as px
+
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) AS CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['PIPELINE NAME','CREDITS CONSUMED'])
+fig = px.line(df, x="PIPELINE NAME", y="CREDITS CONSUMED", title='Analytics for Credits consumed')
+fig.update_xaxes(titlefont=dict(color='crimson',size=10),tickfont=dict(family='Rockwell', size=5))
+fig.update_yaxes(titlefont=dict(color='crimson',size=10))
+
+   
+st.write(fig)
