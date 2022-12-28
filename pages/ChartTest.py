803b37f261d98bba28f07805273ca260fc3d52cf
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 15:55:44 +0530

    Update ChartTest.py

--- pages/ChartTest.py
+++ pages/ChartTest.py
@@ -10,10 +10,12 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['PIPELINE_NAME','PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
-st.write(df)
+df = pd.DataFrame("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+res = my_cur.fetchall()
+st.write(res)
 #df_nxt = df.drop_duplicates(subset='Pipeline Start Time')
 df_wide = df.pivot(index ='PIPELINE_START_TIME', columns = 'PIPELINE_NAME',  values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
-st.write(df_wide)
+#st.write(df_wide)
 fig1 = px.line(
     data_frame = df_wide
     ,x = 'PIPELINE_START_TIME'
