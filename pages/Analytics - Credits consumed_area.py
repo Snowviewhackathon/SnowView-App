5831fedffb91e5fb6e10ee5d1f2f321ebb69c95c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 18:47:40 +0530

    Update Analytics - Credits consumed_area.py

--- pages/Analytics - Credits consumed_area.py
+++ pages/Analytics - Credits consumed_area.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur_new = my_cnx.cursor() 
-my_cur_new.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+my_cur_new.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res_new=my_cur_new.fetchall()
-df_new= pd.DataFrame(res_new, columns=['PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
-df_new = df_new.set_index('PIPELINE_START_TIME')
-st.area_chart(df_new)
+df_new= pd.DataFrame(res_new, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME'])
+fig = px.area(df_new, x='PIPELINE NAME', y='PIPELINE EXECUTION TIME')
+
+st.write(fig)
