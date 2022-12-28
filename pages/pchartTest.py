e042162c14dd9158b5dad39300b4e9d3dbec3ddc
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 11:43:02 +0530

    Update pchartTest.py

--- pages/pchartTest.py
+++ pages/pchartTest.py
@@ -1,21 +1,26 @@
+import snowflake.connector
+import streamlit as st
+st.set_page_config(layout="wide")
 import pandas as pd
-import io, requests
-import plotly.express as px
 
-df = pd.read_csv(
-    io.StringIO(
-        requests.get(
-            "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
-        ).text
-    )
-)
-df["Date"] = pd.to_datetime(df["date"])
-df["Country"] = df["location"]
-df["7day_rolling_avg"] = df["daily_people_vaccinated_per_hundred"]
 
-Date = df[df.Country == "India"].Date
-New_cases = df[df.Country == "India"]["7day_rolling_avg"]
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+#my_cur_new = my_cnx.cursor() 
+#my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
+#my_cur_new.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+#res_new=my_cur_new.fetchall()
+#df_new= pd.DataFrame(res_new, columns=[' PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+#df_new = df_new.set_index(' PIPELINE_START_TIME')
+#st.area_chart(df_new)
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
+res = my_cur.fetchall()
+#df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+df= pd.DataFrame(res, columns=['PIPELINE_NAME','Pipeline Start Time','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+#df = df.set_index('PIPELINE_NAME')
+st.write(df)
+df_wide = df.pivot(index = 'Pipeline Start Time', columns = 'PIPELINE_NAME', values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
+st.write(df_wide)
+#st.line_chart(df)
 
-px.line(df, x=Date, y=New_cases, title="India Daily New Covid Cases").update_layout(
-    xaxis_title="Date", yaxis_title="7 day avg"
-)
+
