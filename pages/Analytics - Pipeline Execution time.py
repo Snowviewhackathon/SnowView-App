f10406d76a7bf6610e096d983aa6a79558957d4d
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 18:32:00 +0530

    Update Analytics - Pipeline Execution time.py

--- pages/Analytics - Pipeline Execution time.py
+++ pages/Analytics - Pipeline Execution time.py
@@ -8,10 +8,10 @@ import plotly.express as px
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor() 
-my_cur.execute("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) AS CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+my_cur.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['PIPELINE NAME','CREDITS CONSUMED'])
-fig = px.line(df, x="PIPELINE NAME", y="CREDITS CONSUMED", title='Analytics for Credits consumed')
+df= pd.DataFrame(res, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME'])
+fig = px.line(df, x="PIPELINE NAME", y="PIPELINE EXECUTION TIME", title='Analytics for Pipeline Execution time')
 fig.update_xaxes(titlefont=dict(color='crimson',size=10),tickfont=dict(family='Rockwell', size=5))
 fig.update_yaxes(titlefont=dict(color='crimson',size=10))
 
