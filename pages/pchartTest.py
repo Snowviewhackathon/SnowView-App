68368707ff2b4ed3be2905d57bd55d0290b17ce6
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 14:43:50 +0530

    Update pchartTest.py

--- pages/pchartTest.py
+++ pages/pchartTest.py
@@ -10,30 +10,21 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
 #df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
-df= pd.DataFrame(res, columns=['PIPELINE_NAME','Pipeline Start Time','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+df= pd.DataFrame(res, columns=['PIPELINE_NAME','PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
 #df = df.set_index('PIPELINE_NAME')
 #st.write(df)
 #df_nxt = df.drop_duplicates(subset='Pipeline Start Time')
-df_wide = df.pivot(index = 'Pipeline Start Time', columns = 'PIPELINE_NAME', values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
+df_wide = df.pivot(index = 'PIPELINE_START_TIME', columns = 'PIPELINE_NAME', values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
 st.write(df_wide)
 fig1 = px.line(
     data_frame = df_wide
-    ,x = 'Pipeline Start Time'
+    ,x = 'PIPELINE_START_TIME'
     ,y = ['Snowpark : Snowpark Python Code','Task : TASK_LOAD_AUDIT_HISTORY','Task : TASK_SP_LOAD_CSV_EXTERNAL_STAGE','Task : TASK_SP_LOAD_CSV_INTERNAL_STAGE']
 )
  
 #fig1.show()
 st.write(fig1)
-my_cur_new = my_cnx.cursor() 
-#my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_VW")
-my_cur_new.execute("SELECT PIPELINE_STATUS,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
-res_new=my_cur_new.fetchall()
-df_neww= pd.DataFrame(res_new, columns=[ 'PIPELINE_STATUS','PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
-#df_neww = df_neww.drop_duplicates(subset='PIPELINE_START_TIME')
-st.write(df_neww)
-fig2 = px.area(df_neww, x="PIPELINE_START_TIME", y="CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION", color="PIPELINE_STATUS")
-#, line_group="country")
-st.write(fig2)
+
 
 
 
