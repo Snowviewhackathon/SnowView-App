99354e88c1ac2aa00c9a5628b7a37db64833ae0c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 17:41:08 +0530

    Update testNew.py

--- pages/testNew.py
+++ pages/testNew.py
@@ -9,12 +9,14 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor() 
 my_cur.execute("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) AS CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['PIPELINE_NAME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
+df= pd.DataFrame(res, columns=['PIPELINE NAME','CREDITS CONSUMED'])
 fig = px.line(df, x="PIPELINE NAME", y="CREDITS CONSUMED", title='Analytics for Credits consumed')
 fig.update_layout(
     autosize=False,
     width=500,
     height=500,
-)
+    yaxis=dict(
+            titlefont=dict(size=30),
+        ))
 st.write(fig)
 
