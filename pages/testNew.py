995752eaef9a03f37db7cdd97c5030d0f2884d49
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 17:35:56 +0530

    Update testNew.py

--- pages/testNew.py
+++ pages/testNew.py
@@ -10,10 +10,12 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT PIPELINE_NAME,SUM(CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION) AS CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['PIPELINE_NAME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
-fig = px.line(df, x="PIPELINE_NAME", y="CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION", title='Life expectancy in Canada')
-#fig.show()
-
-#fig3.show()
-#fig1.show()
+fig = px.line(df, x="PIPELINE_NAME", y="CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION", title='Analytics for Credits consumed')
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    paper_bgcolor="LightSteelBlue",
+)
 st.write(fig)
 
