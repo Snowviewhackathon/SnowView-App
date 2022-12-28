76a0ee42c6846c21285571d4613ba3fd8dacdbe6
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 12:32:10 +0530

    Update pchartTest.py

--- pages/pchartTest.py
+++ pages/pchartTest.py
@@ -14,7 +14,7 @@ my_cur = my_cnx.cursor()
 #df_new= pd.DataFrame(res_new, columns=[' PIPELINE_START_TIME','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
 #df_new = df_new.set_index(' PIPELINE_START_TIME')
 #st.area_chart(df_new)
-my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_HISTORY")
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW")
 res = my_cur.fetchall()
 #df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
 df= pd.DataFrame(res, columns=['PIPELINE_NAME','Pipeline Start Time','CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION'])
@@ -24,7 +24,7 @@ df_nxt = df.drop_duplicates(subset='Pipeline Start Time')
 df_wide = df_nxt.pivot(index = 'Pipeline Start Time', columns = 'PIPELINE_NAME', values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
 st.write(df_wide)
 fig1 = px.line(
-    data_frame = df_nxt
+    data_frame = df_wide
     ,x = 'Pipeline Start Time'
     ,y = ['Snowpark : Snowpark Python Code','Task : TASK_LOAD_AUDIT_HISTORY','Task : TASK_SP_LOAD_CSV_EXTERNAL_STAGE','Task : TASK_SP_LOAD_CSV_INTERNAL_STAGE']
 )
