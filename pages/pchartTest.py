1d1353580b05f08deef0479929df2a1af37f6fbf
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-28 12:04:41 +0530

    Update pchartTest.py

--- pages/pchartTest.py
+++ pages/pchartTest.py
@@ -21,10 +21,11 @@ df= pd.DataFrame(res, columns=['PIPELINE_NAME','Pipeline Start Time','CREDITS_CO
 #df = df.set_index('PIPELINE_NAME')
 st.write(df)
 df_wide = df.pivot(index = 'Pipeline Start Time', columns = 'PIPELINE_NAME', values = 'CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION').reset_index()
+st.write(df_wide)
 fig1 = px.line(
     data_frame = df_wide
     ,x = 'Pipeline Start Time'
-    ,y = ['Snowpark : Snowpark Python Code','Task : TASK_LOAD_AUDIT_HISTORY']
+    ,y = ['Snowpark : Snowpark Python Code','Task : TASK_LOAD_AUDIT_HISTORY','Task : TASK_SP_LOAD_CSV_EXTERNAL_STAGE','Task : TASK_SP_LOAD_CSV_INTERNAL_STAGE']
 )
  
 #fig1.show()
