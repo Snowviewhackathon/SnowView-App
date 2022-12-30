491a10e868293d9e1760bd7e1de521454b2f4abc
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-30 19:38:48 +0530

    Update Analytics for Pipeline Execution time.py

--- pages/Analytics for Pipeline Execution time.py
+++ pages/Analytics for Pipeline Execution time.py
@@ -9,22 +9,18 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor() 
 my_cur.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res = my_cur.fetchall()
-df= pd.DataFrame(res, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME'])
-fig = px.line(df, x="PIPELINE NAME", y="PIPELINE EXECUTION TIME", title='Analytics for Pipeline Execution time')
-fig.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=5))
-fig.update_yaxes(titlefont=dict(color='crimson',size=10))
-
-   
+df= pd.DataFrame(res, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME(in seconds)'])
+fig = px.line(df, x="PIPELINE NAME", y="PIPELINE EXECUTION TIME(in seconds)", title='Analytics for Pipeline Execution time')
+fig.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=7))
+fig.update_yaxes(titlefont=dict(color='crimson',size=10))  
 st.write(fig)
 
 
 my_cur2 = my_cnx.cursor() 
 my_cur2.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur2.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
+my_cur2.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
 res2 = my_cur2.fetchall()
-df2= pd.DataFrame(res2, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
-
-
+df2= pd.DataFrame(res2, columns=['Pipeline Name','Pipeline Execution Time (in seconds)'])
 fig1 = px.pie(df2, values='Pipeline Execution Time (in seconds)', names='Pipeline Name',title='Analytics for Pipeline Execution time')
 fig1.update_traces(textposition='inside')
 fig1.update_layout(legend_title_text='Pipeline Names')
@@ -34,5 +30,4 @@ fig1.update_layout(showlegend=True, legend=dict(
         size=8
     )
 ))
-
 st.write(fig1)
