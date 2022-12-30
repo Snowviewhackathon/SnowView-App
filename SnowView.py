06eec61651e732c54bef836916a6a8bbf21723a2
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2022-12-30 18:51:56 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -1,14 +1,11 @@
 import pickle
 from pathlib import Path
-from Logging import st_logging as log
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 
-log_obj = log.St_logging()
-logger = log_obj.create_log()
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+
 st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:", layout="wide")
 
 hide_bar= """
@@ -24,9 +21,9 @@ hide_bar= """
 """
 
 # --- USER AUTHENTICATION ---
-names = ["Aniket", "Venu"]
-usernames = ["Aniket_S", "Venu_B"]
-logger.info("successfully logged in")
+names = ["Aniket", "Venu","Pooja]
+usernames = ["Aniket_S", "Venu_B",Pooja_M]
+
 # load hashed passwords
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
@@ -36,7 +33,6 @@ authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
     "SIPL_dashboard", "abcdef")
 
 name, authentication_status, username = authenticator.login("Login", "main")
-#logger.info(f"successfully logined as user {name}")
 
 if authentication_status == False:
     st.error("Username/password is incorrect")
@@ -50,14 +46,6 @@ if authentication_status == None:
 if authentication_status:
     # # ---- SIDEBAR ----
     st.sidebar.title(f"Welcome {name}")
-    # st.sidebar.header("select page here :")
-    st.write("# Welcome to SnowView!..")
-
-    ###about ....
-    #st.subheader("Introduction :")
-    #st.text("1. \n2. \n3. \n4. \n5. \n")
-
-    #st.sidebar.success("Select a page above.")
 
     ###---- HIDE STREAMLIT STYLE ----
     hide_st_style = """
@@ -68,7 +56,5 @@ if authentication_status:
                 </style>
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
-
     authenticator.logout("Logout", "sidebar")
-
 st.snow()
