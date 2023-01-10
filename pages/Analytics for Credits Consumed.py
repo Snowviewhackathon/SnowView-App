c41b2815242114385c614b3fa4081b17836e28cb
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-10 16:42:52 +0530

    Update Analytics for Credits Consumed.py

--- pages/Analytics for Credits Consumed.py
+++ pages/Analytics for Credits Consumed.py
@@ -27,23 +27,3 @@ fig1.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tick
 fig1.update_yaxes(titlefont=dict(color='crimson',size=10))
 #st.write(fig)
 st.write(fig1)
-
-my_cur2 = my_cnx.cursor() 
-#my_cur2.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur2.execute("select * from pipeline_credits_consumed_vw")
-#my_cur2.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
-res2 = my_cur2.fetchall()
-df2= pd.DataFrame(res2, columns=['Pipeline Name','Credits Consumed'])
-fig2 = px.pie(df2, values='Credits Consumed', names='Pipeline Name',title='Credits Consumed by Pipelines')
-fig2.update_traces(textposition='inside')
-fig2.update_layout(legend_title_text='Pipeline Names')
-fig2.update_layout(showlegend=True, legend=dict(
-    title_font_family='Courier New',
-    font=dict(
-        size=10
-    )
-))
-st.write(fig2)
-
-
-
