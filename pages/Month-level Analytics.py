c6be0a2e18445c482ea0c0ab3184c14ed7ab6bec
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-11 20:05:22 +0530

    Create Month-level Analytics.py

---
+++ pages/Month-level Analytics.py
@@ -0,0 +1,18 @@
+import snowflake.connector
+import streamlit as st
+st.set_page_config(layout="wide")
+import pandas as pd
+import plotly.express as px
+
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cur = my_cnx.cursor() 
+my_cur.execute("SELECT * from MONTHLY_CREDITS_CONSUMED_VIEW")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['MONTH','CREDITS CONSUMED'])
+#fig = px.line(df, x="MONTH", y="CREDITS CONSUMED", title='Analytics for Pipeline Execution time')
+#fig.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=7))
+#fig.update_yaxes(titlefont=dict(color='crimson',size=10))  
+
+fig = px.bar(df, x="MONTH", y="CREDITS CONSUMED")
+st.write(fig)
