7715c66fbc10dedcdbc24e879853fd32639e0bb6
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-11 20:31:58 +0530

    Update Analytics for Credits Consumed.py

--- pages/Analytics for Credits Consumed.py
+++ pages/Analytics for Credits Consumed.py
@@ -17,30 +17,30 @@ fig.update_yaxes(titlefont=dict(color='crimson',size=10))
 #fig.update_xaxes(tickangle=90)
 st.write(fig)
 
-my_cur1 = my_cnx.cursor() 
-my_cur1.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW ")
-res1=my_cur1.fetchall()
-df1= pd.DataFrame(res1, columns=['PIPELINE START TIME','CREDITS CONSUMED'])
-fig1 = px.area(df1, x='PIPELINE START TIME', y='CREDITS CONSUMED',title='Credits Consumed over Time Period')
-fig1.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=10))
-fig1.update_yaxes(titlefont=dict(color='crimson',size=10))
-st.write(fig1)
+#my_cur1 = my_cnx.cursor() 
+my_cur.execute("SELECT PIPELINE_START_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION FROM SNOWVIEW_AUDIT_VW ")
+res=my_cur.fetchall()
+df= pd.DataFrame(res, columns=['PIPELINE START TIME','CREDITS CONSUMED'])
+fig = px.area(df, x='PIPELINE START TIME', y='CREDITS CONSUMED',title='Credits Consumed over Time Period')
+fig.update_xaxes(titlefont=dict(family='Rockwell',color='crimson',size=10),tickfont=dict(family='Rockwell', size=10))
+fig.update_yaxes(titlefont=dict(color='crimson',size=10))
+st.write(fig)
 
-my_cur2 = my_cnx.cursor() 
-my_cur2.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur2.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
-res2 = my_cur2.fetchall()
-df2= pd.DataFrame(res2, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
-fig2 = px.pie(df2, values='Credits Consumed', names='Pipeline Name',title='Credits Consumed by Pipelines')
-fig2.update_traces(textposition='inside')
-fig2.update_layout(legend_title_text='Pipeline Names')
-fig2.update_layout(showlegend=True, legend=dict(
+#my_cur2 = my_cnx.cursor() 
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT PIPELINE_NAME,PIPELINE_EXECUTOR,PIPELINE_STATUS,PIPELINE_START_TIME,PIPELINE_END_TIME,PIPELINE_EXECUTION_TIME,CREDITS_CONSUMED_FOR_PIPELINE_EXECUTION,ERROR_DETAILS FROM SNOWVIEW_AUDIT_HISTORY ORDER BY PIPELINE_START_TIME DESC")
+res = my_cur.fetchall()
+df= pd.DataFrame(res, columns=['Pipeline Name','Pipeline Executor','Pipeline Status','Pipeline Start Time','Pipeline End Time','Pipeline Execution Time (in seconds)','Credits Consumed','Error Details'])
+fig = px.pie(df, values='Credits Consumed', names='Pipeline Name',title='Credits Consumed by Pipelines')
+fig.update_traces(textposition='inside')
+fig.update_layout(legend_title_text='Pipeline Names')
+fig.update_layout(showlegend=True, legend=dict(
     title_font_family='Courier New',
     font=dict(
         size=8
     )
 ))
-st.write(fig2)
+st.write(fig)
 
 
 
