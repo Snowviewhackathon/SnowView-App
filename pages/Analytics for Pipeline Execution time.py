ea3c3061fa268b535d09871810486fd40f3f4011
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-11 11:40:43 +0530

    Update Analytics for Pipeline Execution time.py

--- pages/Analytics for Pipeline Execution time.py
+++ pages/Analytics for Pipeline Execution time.py
@@ -7,8 +7,8 @@ import plotly.express as px
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor() 
-my_cur.execute("select * from pipeline_execution_time_vw")
-#my_cur.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
+#my_cur.execute("select * from pipeline_execution_time_vw")
+my_cur.execute("SELECT PIPELINE_NAME,SUM(PIPELINE_EXECUTION_TIME) AS PIPELINE_EXECUTION_TIME FROM SNOWVIEW_AUDIT_VW GROUP BY PIPELINE_NAME")
 res = my_cur.fetchall()
 df= pd.DataFrame(res, columns=['PIPELINE NAME','PIPELINE EXECUTION TIME(in seconds)'])
 fig = px.line(df, x="PIPELINE NAME", y="PIPELINE EXECUTION TIME(in seconds)", title='Analytics for Pipeline Execution time')
