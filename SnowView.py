c04ba7a861581602c19498369734fae51d350a2a
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-12 09:45:48 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -4,8 +4,6 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
-
-
 st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:")
 
 hide_bar= """
