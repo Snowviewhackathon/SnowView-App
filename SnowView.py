65f5ca162388c2787568d8d5b6fc469a0b12e27c
Snowviewhackathon <120005257+Snowviewhackathon@users.noreply.github.com>
2023-01-12 09:47:55 +0530

    Update SnowView.py

--- SnowView.py
+++ SnowView.py
@@ -47,13 +47,7 @@ if authentication_status:
     st.write("# Welcome to SnowView!..")
 
     ###---- HIDE STREAMLIT STYLE ----
-    hide_st_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                header {visibility: hidden;}
-                </style>
-                """
+    
     st.markdown(hide_st_style, unsafe_allow_html=True)
     authenticator.logout("Logout", "sidebar")
     st.snow()
